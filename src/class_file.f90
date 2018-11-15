5efa35bcecdb9f37a95a4020f1abb02a53fc4606
komahan <komahan.cool@gmail.com>
2018-11-15 18:17:52 -0500

    fixed read line to simpler logic

--- src/class_file.f90
+++ src/class_file.f90
@@ -15,6 +15,7 @@ module class_file
 
      character(:), allocatable :: filename ! filename
      type(integer) :: file_unit            ! file unit number
+     integer       :: buffer_size
 
    contains
 
@@ -42,14 +43,22 @@ contains
   ! Construct a file object with filename
   !===================================================================!
   
-  type(file) function create(filename) result (this)
+  type(file) function create(filename, line_width) result (this)
 
-    type(character(*)), intent(in) :: filename
+    type(character(*)), intent(in)           :: filename
+    type(integer)     , intent(in), optional :: line_width
     logical :: ok
     integer :: i
 
     ! Set the file name
     allocate(this % filename, source=filename)
+
+    ! Line width
+    if (present(line_width)) then
+       this % buffer_size = line_width
+    else
+       this % buffer_size = 100
+    end if
 
     ! Use an available handle for opening
     i = 99
@@ -63,11 +72,11 @@ contains
     end do check_unit
 
   end function create
-  
+
   !===================================================================!
   ! Open the file
   !===================================================================!
-  
+
   subroutine open(this)
 
     class(file), intent(in) :: this
@@ -91,7 +100,7 @@ contains
   !===================================================================!
   ! return the file unit number
   !===================================================================!
-  
+
   pure type(integer) function get_unit(this)
 
     class(file), intent(in) :: this
@@ -100,130 +109,71 @@ contains
 
   end function get_unit
 
-    !===================================================================!
-    ! Utility function for get number of lines in mesh file
-    !===================================================================!
-    
-    type(integer) function get_num_lines(this) result(nlines)
+  !===================================================================!
+  ! Utility function for get number of lines in mesh file
+  !===================================================================!
 
-      class(file) , intent(in) :: this
-      integer :: stat
+  type(integer) function get_num_lines(this) result(nlines)
 
-      nlines = 0 
-      call this % open()
-      do
-         read(this % get_unit(),*, iostat=stat)
-         if (stat .ne. 0) exit
-         nlines = nlines + 1
-      end do
-      call this % close()
+    class(file) , intent(in) :: this
+    integer :: stat
 
-    end function get_num_lines
+    nlines = 0 
+    call this % open()
+    do
+       read(this % get_unit(),*, iostat=stat)
+       if (stat .ne. 0) exit
+       nlines = nlines + 1
+    end do
+    call this % close()
 
-    !=================================================================!
-    ! Read all lines and return a string array
-    !=================================================================!
-    
-    subroutine read_lines(this, lines)
+  end function get_num_lines
 
-      ! Arguments
-      class(file)  , intent(in)            :: this
-      type(string) , allocatable, intent(out) :: lines(:)
+  !=================================================================!
+  ! Read one line and return a string object
+  !=================================================================!
 
-      ! Locals
-      integer :: num_lines
-      integer :: iline
+  subroutine read_line(this, line)
 
-      ! Get number of lines in file to allocate space (uses a separate
-      ! handle) fix!
-      num_lines = this % get_num_lines()
-      allocate(lines(num_lines))
+    ! Arguments
+    class(file)  , intent(in)  :: this
+    type(string) , intent(out) :: line
 
-      ! Loop through each line and read and store into lines
-      call this % open()
-      do iline = 1, num_lines
-         call this % read_line(lines(iline))
-      end do
-      call this % close()
+    ! Locals
+    character(len=this % buffer_size) :: buffer
+    integer                           :: iostat
 
-    end subroutine read_lines
-    
-    !=================================================================!
-    ! Read one line and return a string object
-    !=================================================================!
-!!$    
-!!$    subroutine read_line(this, line)
-!!$
-!!$      ! Arguments
-!!$      class(file)  , intent(in)  :: this
-!!$      type(string) , intent(out) :: line
-!!$      character(len=1024) :: buffer(:)
-!!$      integer :: iostat, isize
-!!$
-!!$      print *, 'reading line from unit', this % get_unit()
-!!$
-!!$      read( this % get_unit(), &
-!!$           & iostat  = iostat, &
-!!$           & fmt     = '(a)' , &
-!!$           & advance = 'no'  , &
-!!$           & size    = isize) buffer
-!!$      
-!!$      print *, 'size of line', isize, 'iostat', iostat, buffer
-!!$
-!!$      line = string(buffer)
-!!$
-!!$    end subroutine read_line
-!!$    
-    subroutine read_line(this, linestr)
+    read(this % get_unit(), fmt = '(a)') buffer
+    line = string(trim(buffer))
 
-      ! Arguments
-      class(file)  , intent(in)    :: this
-      type(string) , intent(out)   :: linestr
+  end subroutine read_line
 
-      ! Locals
-      character(len=:) , allocatable :: line
-      integer          , parameter   :: buflen=1024
-      character(len=buflen)          :: buffer
-      integer                        :: last
-      integer                        :: isize, ier
+  !=================================================================!
+  ! Read all lines and return a string array
+  !=================================================================!
 
-      isize = 0
-      line = ''
-      ier = 0
+  subroutine read_lines(this, lines)
 
-      ! read characters from line and append to result
-      INFINITE: do
-         ! read next buffer (an improvement might be to use stream I/O
-         ! for files other than stdin so system line limit is not
-         ! limiting)
-         read(this % get_unit(),iostat=ier,fmt='(a)',advance='no',size=isize) buffer
-         ! append what was read to result
-         if(isize.gt.0)line=line//buffer(:isize)
-         ! if hit EOR reading is complete unless backslash ends the line
-         if(is_iostat_eor(ier))then
-            last=len(line)
-            if(last.ne.0)then
-               ! if line ends in backslash it is assumed a continued line
-               if(line(last:last).eq."\")then
-                  ! remove backslash
-                  line=line(:last-1)
-                  ! continue on and read next line and append to result
-                  cycle INFINITE
-               endif
-            endif
-            ! hitting end of record is not an error for this routine
-            ier=0
-            ! end of reading line
-            exit INFINITE
-            ! end of file or error
-         elseif(ier.ne.0)then
-            exit INFINITE
-         endif
-      enddo INFINITE
+    ! Arguments
+    class(file)  , intent(in)            :: this
+    type(string) , allocatable, intent(out) :: lines(:)
 
-      line = trim(line)
-      linestr = string(line)
+    ! Locals
+    integer :: num_lines
+    integer :: iline
 
-    end subroutine read_line
+    ! Get number of lines in file to allocate space (uses a separate
+    ! handle) fix!
+    num_lines = this % get_num_lines()
+    allocate(lines(num_lines))
 
+    ! Loop through each line and read and store into lines
+    call this % open()
+    do iline = 1, num_lines
+       call this % read_line(lines(iline))
+    end do
+    call this % close()
+
+  end subroutine read_lines
+   
 end module class_file

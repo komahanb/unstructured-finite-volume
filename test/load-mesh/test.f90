809a669347771b85e101dc7e986caf7e20d13c56
komahan <komahan.cool@gmail.com>
2018-11-20 11:11:52 -0500

    cleanup of test program for string, file, gmsh_loader

--- test/load-mesh/test.f90
+++ test/load-mesh/test.f90
@@ -3,41 +3,73 @@
 !=====================================================================!
 
 program test_mesh
-
-  use class_mesh_loader , only : mesh_loader
+  
   use class_mesh        , only : mesh
-!!$  use class_file        , only : file
-!!$  use class_string      , only : string
+  use class_gmsh_loader , only : gmsh_loader
+
+  use class_file        , only : file
+  use class_string      , only : string
 
   implicit none
 
-  character(len=*), parameter :: filename = 'rectangle.msh'
-  type(mesh_loader) :: gmsh_loader
-  type(mesh)        :: grid
+  !===================================================================!
+  ! Test the functionalities of Class String and Class File
+  !===================================================================!
 
-!!$  type(file)        :: ofile
-!!$
-!!$  type(string), allocatable :: lines(:)
-!!$  integer :: iline
-!!$
-!!$  ofile = file(filename)
-!!$  allocate(lines(ofile % get_num_lines()))
-!!$  call ofile % open()
-!!$  do iline = 1, 42
-!!$     call ofile % read_line(lines(iline))
-!!$  end do
-!!$  call ofile % close()
-!!$  call lines % print()
-!!$  deallocate(lines)
-!!$
-!!$  call ofile % read_lines(lines)
-!!$  call lines % print()
+  test_file_string : block
 
-  ! Create a mesh loader for mesh file
-  gmsh_loader = mesh_loader(filename)
+    character(len=*), parameter :: filename = 'rectangle.msh'
+    type(string), allocatable :: lines(:)
+    type(file)                :: file_obj
+    integer                   :: iline, num_lines
 
-  ! Get the mesh using loader object
-  grid = mesh(gmsh_loader)
-  !call grid % to_string()
+    ! Create a file object
+    file_obj = file(filename)
+    
+    ! Do a line by line read and print contents
+    num_lines = file_obj % get_num_lines()
+    allocate(lines(num_lines))
+    call file_obj % open()
+    do iline = 1, num_lines
+       call file_obj % read_line(lines(iline))
+    end do
+    call file_obj % close()
+    call lines % print()
+    deallocate(lines)
+
+    ! Read everything and print content
+    call file_obj % read_lines(lines)
+    call lines % print()  
+
+  end block test_file_string
+
+  !===================================================================!
+  ! Test the functionalities of Class GMSH_LOADER
+  !===================================================================!
+
+  test_gmsh: block
+
+    character(len=*), parameter :: filename = 'rectangle.msh'
+
+    call test_gmsh_loader(filename)
+
+  end block test_gmsh
+  
+contains
+
+  subroutine test_gmsh_loader(filename)
+
+    character(len=*), intent(in) :: filename
+    type(gmsh_loader) :: gmsh_loader_obj
+    type(mesh)        :: mesh_obj
+
+    ! Create a mesh loader for mesh file
+    gmsh_loader_obj =  gmsh_loader(filename)
+
+    ! Get the mesh using loader object
+    mesh_obj = mesh(gmsh_loader_obj)
+    call mesh_obj % to_string()
+
+  end subroutine test_gmsh_loader
 
 end program test_mesh

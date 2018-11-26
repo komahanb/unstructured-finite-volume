7762bfad183ac1fccebb15c9fd75e6824f523c21
komahan <komahan.cool@gmail.com>
2018-11-26 13:20:34 -0500

    added assembler prototype and CG solver

---
+++ src/class_assembler.f90
@@ -0,0 +1,253 @@
+module class_assembler
+
+  ! import dependencies
+  use iso_fortran_env, only : dp => REAL64
+  use class_mesh, only : mesh
+  
+  implicit none
+
+  private
+  public :: assembler
+  public :: solve_conjugate_gradient
+
+  !===================================================================!
+  ! Class responsible for matrix, right hand side assembly and boundary
+  ! conditions
+  !===================================================================!
+
+  type :: assembler
+
+     type(mesh), pointer :: grid
+     logical :: symmetry = .false.
+
+   contains
+
+     ! Evaluation routines
+     !procedure :: evaluate_vertex_flux
+     !procedure :: evaluate_face_flux
+
+     ! Assembly Routines
+     procedure :: get_source
+     !procedure :: add_skew_source
+     procedure :: get_jacobian_vector_product
+     procedure :: get_transpose_jacobian_vector_product
+     procedure :: write_solution
+     
+  end type assembler
+
+  interface assembler
+     module procedure construct
+  end interface assembler
+
+contains
+    
+  !===================================================================!
+  ! Constructor for physics
+  !===================================================================!
+  
+  type(assembler) function construct(grid) result (this)
+
+    type(mesh), intent(in) :: grid
+
+    print *, "constructing assembler"
+
+    ! Set mesh
+    allocate(this % grid, source  = grid)
+
+    ! Non symmetric jacobian
+    this % symmetry = .false.
+
+  end function construct
+
+  subroutine get_jacobian_vector_product(this, x, Ax)
+
+    class(assembler) , intent(in)    :: this
+    real(dp)         , intent(in)    :: x(:)
+    real(dp)         , intent(out)   :: Ax(:)
+
+    integer               :: n
+    real(dp), allocatable :: A(:,:)
+
+    n = size(x,dim=1)
+    allocate(A(n,n))
+
+    A(1,:) = [2.0d0,1.0d0,1.0d0]
+    A(2,:) = [1.0d0,2.0d0,1.0d0]
+    A(3,:) = [0.0d0,1.0d0,2.0d0]
+
+    Ax = matmul(A,x)
+
+    deallocate(A)
+
+  end subroutine get_jacobian_vector_product
+  
+  subroutine get_transpose_jacobian_vector_product(this, x, Ax)
+
+    class(assembler) , intent(in)    :: this
+    real(dp)         , intent(in)    :: x(:)
+    real(dp)         , intent(out)   :: Ax(:)
+    real(dp), allocatable :: A(:,:)
+    integer               :: n
+
+    n = size(x)
+    allocate(A(n,n))
+
+    A(1,:) = [2.0d0,1.0d0,1.0d0]
+    A(2,:) = [1.0d0,2.0d0,1.0d0]
+    A(3,:) = [0.0d0,1.0d0,2.0d0]
+
+    Ax = matmul(transpose(A),x)
+
+    deallocate(A)
+
+  end subroutine get_transpose_jacobian_vector_product
+  
+  subroutine get_source(this, b)
+
+    class(assembler), intent(in)  :: this
+    real(dp)        , intent(out) :: b(:)
+
+    b(1) = 1.0d0
+    b(2) = 1.0d0
+    b(3) = 1.0d0
+
+  end subroutine get_source
+
+  subroutine solve_conjugate_gradient(oassembler, max_it, max_tol, x)
+
+    ! Arguments
+    type(assembler)  , intent(in)    :: oassembler
+    integer          , intent(in)    :: max_it
+    real(dp)         , intent(in)    :: max_tol
+    real(dp)         , intent(inout) :: x(:)
+
+    ! Create local data
+    real(dp), allocatable :: p(:), r(:), w(:), Ax(:), tmp(:)
+    real(dp), allocatable :: b(:)
+    real(dp)              :: alpha, beta
+    real(dp)              :: bnorm, rnorm
+    real(dp)              :: tol
+    integer               :: iter
+    real(dp)              :: rho(2)
+
+    ! Start the iteration counter
+    iter = 1
+
+    ! Memory allocations
+    allocate(b,p,r,w,Ax,tmp,mold=x)
+
+    ! Norm of the right hand side
+    call oassembler % get_source(tmp)
+    if (oassembler % symmetry .eqv. .false.) then
+       call oassembler % get_transpose_jacobian_vector_product(tmp, b)
+    end if
+    bnorm = norm2(b)
+
+    ! Norm of the initial residual
+    call oassembler % get_jacobian_vector_product(x, tmp)
+    if (oassembler % symmetry .eqv. .false.) then
+       call oassembler % get_transpose_jacobian_vector_product(tmp, Ax)
+    end if
+    r         = b - Ax
+    rnorm     = norm2(r)
+    tol       = rnorm/bnorm
+    rho(2)    = rnorm*rnorm
+    
+    open(10, file='cg.log', action='write', position='append')
+
+    tol = huge(1.0_dp)
+
+    ! Apply Iterative scheme until tolerance is achieved
+    do while ((tol .gt. max_tol) .and. (iter .lt. max_it))
+
+       ! step (a) compute the descent direction
+       if ( iter .eq. 1) then
+          ! steepest descent direction p
+          p = r
+       else
+          ! take a conjugate direction
+          beta = rho(2)/rho(1)
+          p = r + beta*p
+       end if
+
+       ! step (b) compute the solution update
+       call oassembler % get_jacobian_vector_product(p, tmp)
+       if (oassembler % symmetry .eqv. .false.) then
+          call oassembler % get_transpose_jacobian_vector_product(tmp, w)
+       end if
+       !w = matmul(A,p)
+
+       ! step (c) compute the step size for update
+       alpha = rho(2)/dot_product(p, w)
+
+       ! step (d) Add dx to the old solution
+       x = x + alpha*p
+
+       ! step (e) compute the new residual
+       r = r - alpha*w
+
+       ! step(f) update values before next iteration
+       rnorm = norm2(r)
+       tol = rnorm/bnorm
+
+       write(10,*) iter, tol
+       print *, iter, tol, rnorm, rho
+
+       iter = iter + 1
+
+       rho(1) = rho(2)
+       rho(2) = rnorm*rnorm
+
+    end do
+
+    close(10)
+
+    deallocate(r, p, w, b)
+
+  end subroutine solve_conjugate_gradient
+  
+  !===================================================================!
+  ! Write solution to file
+  !===================================================================!
+  
+  subroutine write_solution(this, filename)!, phi)
+
+    class(assembler)              :: this
+    character(len=*), intent(in)  :: filename
+    character(len=:), allocatable :: path
+    character(len=:), allocatable :: new_name
+!    real(dp)        , intent(in)  :: phi(:)
+    integer                       :: k, j, i, ierr
+
+    ! Open resource
+    path = trim(filename)
+
+    open(unit=90, file=trim(path), iostat= ierr)
+    if (ierr .ne. 0) then
+       write(*,'("  >> Opening file ", 39A, " failed")') path
+       return
+    end if
+
+    ! Write header
+    write(90, *) 'TITLE = "FVM-Laplace"'
+    write(90, *) 'VARIABLES = "x" "y"'
+    write(90, *) 'ZONE T="Temperature", N=', this % grid % num_vertices, &
+         & ', E=', this % grid % num_cells, &
+         & ', DATAPACKING=POINT, ZONETYPE=FEQUADRILATERAL'
+    
+    ! Write vertices
+    do i = 1, this % grid % num_vertices
+       write(90,*) this % grid % vertices(1:2,i)!, phi(i)
+    end do
+    
+    ! Write cell connectivities
+    do i = 1, this % grid % num_cells
+       write(90,*) this % grid % cell_vertices(:,i)
+    end do
+
+    ! Close resource
+    close(90)
+
+  end subroutine write_solution
+
+end module class_assembler
---
+++ test/assembly/test.f90
@@ -0,0 +1,54 @@
+!=====================================================================!
+! Test loading of mesh and mesh pre-processing
+!=====================================================================!
+
+program test_mesh
+
+  use iso_fortran_env   , only : dp => REAL64
+  use class_gmsh_loader , only : gmsh_loader
+  use class_mesh        , only : mesh
+  use class_assembler   , only : assembler, solve_conjugate_gradient
+
+  implicit none
+  
+  character(len=*)  , parameter   :: filename = "rectangle.msh"
+  class(gmsh_loader), allocatable :: gmsh_loader_obj
+  class(mesh)       , allocatable :: mesh_obj
+
+  ! Solution parameters
+  integer  , parameter   :: npts = 3
+  real(dp) , parameter   :: max_tol = 1.0d-8
+  integer  , parameter   :: max_it = 100
+  real(dp) , allocatable :: x(:)
+  class(assembler), allocatable :: FVMAssembler
+
+  ! Create a mesh object
+  allocate(gmsh_loader_obj, source =  gmsh_loader(filename))
+  allocate(mesh_obj, source = mesh(gmsh_loader_obj))
+  call mesh_obj % to_string()
+  deallocate(gmsh_loader_obj)
+
+  ! Create an assembler object for assembling the linear system
+  ! Geometry and meshing
+
+  allocate(FVMAssembler, source = assembler(mesh_obj))
+
+  ! Writes the mesh for tecplot
+  call FVMassembler % write_solution("mesh.dat")
+  
+  ! Create a solver object to solve the linear system
+  
+  ! Solve (wrap this into a solver object)
+  allocate(x(npts))
+  x = 1.0d0
+  !call random_number(x)
+  print *, 'xinit', x
+  call solve_conjugate_gradient(FVMAssembler, max_it, max_tol, x)
+  print *, 'solution', x
+  
+  deallocate(mesh_obj)
+  deallocate(FVMAssembler)
+
+  contains
+
+end program test_mesh
